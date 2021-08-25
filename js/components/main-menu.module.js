e8646ba4d0278519c98a6e2b4fa9751b36b24489
Mahmoud Magdy <moudmagdy@hotmail.com>
2021-08-25 17:15:47 +0200

    updated the menu

--- js/components/main-menu.module.js
+++ js/components/main-menu.module.js
@@ -1,11 +1,5 @@
-// const windowHeight = window.innerHeight;
-// const mainMenu = document.querySelector('.main-menu');
 const mainMenuToggles = document.querySelectorAll('.main-menu__toggle');
 const subMenuToggles = document.querySelectorAll('.has__submenu > a');
-// const headerMainMenuToggle = document.querySelector('.page__layout header > .main-menu__toggle');
-// const menuNav = document.querySelector('.main-menu nav');
-// const menuNavItem = document.querySelectorAll('.main-menu nav > a');
-// const menuNavItemsLength = menuNavItem.length;
 
 mainMenuToggles.forEach(mainMenuToggle => {
     mainMenuToggle.addEventListener('click', (e) => {
@@ -13,76 +7,34 @@ mainMenuToggles.forEach(mainMenuToggle => {
         document.querySelector('body').classList.toggle('main-menu--opened');
     });
 });
-subMenuToggles.forEach(subMenuToggle => {
-    subMenuToggle.addEventListener('click', (e) => {
-        e.preventDefault();
-        subMenuToggle.closest('.has__submenu').classList.toggle('submenu--opened');
 
-        if (subMenuToggle.closest('.has__submenu').classList.contains('submenu--opened')) {
-            subMenuToggle.classList.add('active');
-            subMenuToggle.nextElementSibling.style.maxHeight = subMenuToggle.nextElementSibling.scrollHeight + 'px';
-        } else {
-            subMenuToggle.classList.remove('active');
-            subMenuToggle.nextElementSibling.style.maxHeight = 0;
-        }
-    });
-});
+function openSubMenu(e) {
+    e.preventDefault();
 
-// function closeMainMenu(e) {
-//     if (!headerMainMenuToggle.contains(e.target) && (!mainMenu.contains(e.target))) {
-//         document.querySelector('body').classList.remove('main-menu--opened');
-//     }
-// }
+    this.closest('.has__submenu').classList.toggle('submenu--opened');
 
-// function createSubmenu() {
-//     const navMenuItemHeight = 76;
-//     const navMaximumOccupancy = Math.floor(menuNav.offsetHeight / navMenuItemHeight);
-//     const theRestNavElments = document.querySelectorAll('.main-menu nav > a:nth-child(+n+' + navMaximumOccupancy + ')');
-//     if (menuNavItemsLength > navMaximumOccupancy) {
-//         const subMenu = document.createElement('div');
-//         const subMenuToggle = document.createElement('a');
-//         const subMenuContent = document.createElement('div');
+    if (this.closest('.has__submenu').classList.contains('submenu--opened')) {
+        this.classList.add('active');
+        this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 'px';
+    } else {
+        this.classList.remove('active');
+        this.nextElementSibling.style.maxHeight = 0;
+    }
+}
 
-//         if (!menuNav.querySelector('.submenu')) {
-//             subMenu.classList.add('submenu');
-//             subMenuToggle.setAttribute('href', '#');
-//             subMenuToggle.innerHTML = '<svg width="10" height="2" viewBox="0 0 10 2"><path d="M10,1c0,0.6-0.4,1-1,1S8,1.6,8,1s0.4-1,1-1S10,0.4,10,1z M6,1c0,0.6-0.4,1-1,1S4,1.6,4,1s0.4-1,1-1 S6,0.4,6,1z M2,1c0,0.6-0.4,1-1,1S0,1.6,0,1s0.4-1,1-1S2,0.4,2,1z"></path></svg>';
-//             subMenu.appendChild(subMenuToggle);
-//             subMenuContent.classList.add('submenu__content');
-//             subMenu.appendChild(subMenuContent);
-//             menuNav.appendChild(subMenu);
+subMenuToggles.forEach(subMenuToggle => subMenuToggle.addEventListener('click', openSubMenu));
 
-//             subMenuToggle.addEventListener('click', (e) => {
-//                 e.preventDefault();
-//                 subMenu.classList.toggle('submenu-shown');
-//             });
+// subMenuToggles.forEach(subMenuToggle => {
+//     subMenuToggle.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         subMenuToggle.closest('.has__submenu').classList.toggle('submenu--opened');
 
-//             menuNavItem.forEach(item => {
-//                 item.classList.remove('d-none');
-//             });
-
-//             theRestNavElments.forEach(element => {
-//                 const clonedElements = element.cloneNode(true);
-//                 subMenuContent.appendChild(clonedElements);
-//                 // element.remove();
-//                 element.classList.add('d-none');
-//             });
-
+//         if (subMenuToggle.closest('.has__submenu').classList.contains('submenu--opened')) {
+//             subMenuToggle.classList.add('active');
+//             subMenuToggle.nextElementSibling.style.maxHeight = subMenuToggle.nextElementSibling.scrollHeight + 'px';
 //         } else {
-//             menuNavItem.forEach(item => {
-//                 item.classList.remove('d-none');
-//             });
-
-//             document.querySelector('.submenu__content').innerHTML = '';
-
-//             theRestNavElments.forEach(element => {
-//                 const clonedElements = element.cloneNode(true);
-//                 document.querySelector('.submenu__content').appendChild(clonedElements);
-//                 element.classList.add('d-none');
-//             });
+//             subMenuToggle.classList.remove('active');
+//             subMenuToggle.nextElementSibling.style.maxHeight = 0;
 //         }
-//     }
-// }
-// document.addEventListener('click', (e) => document.querySelector('body').classList.contains('main-menu--opened') ? closeMainMenu(e) : "");
-// window.addEventListener('load', createSubmenu);
-// window.addEventListener('resize', createSubmenu);
+//     });
+// });
